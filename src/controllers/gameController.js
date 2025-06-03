968bb770b7452371c67f2f6fbb85b4840ca71fc3
Delcan-f <109348946+Delcan-f@users.noreply.github.com>
2025-06-03 20:18:28 +1000

    feat: completed 2

---
+++ src/controllers/gameController.js
@@ -0,0 +1,32 @@
+const Game = require("../models/Game.js");
+
+exports.createGame = async (request, response) => {
+    try {
+        const game = await Game.create(request.body);
+        response.status(201).json(game);
+    } catch (err) {
+        response.status(400).json({ error: err.message });
+    }
+};
+
+exports.getAllGames = async (request, response) => {
+    try {
+        const games = await Game.find().sort({ date: -1 });
+        response.json(games);
+    } catch (err) {
+        response.status(500).json({ error: err.message });
+    }
+};
+
+exports.getGameStats = async (request, response) => {
+    try {
+        const games = await Game.find();
+        const totalGames = games.length;
+        const bestScore = Math.min(...games.map(g => g.scores.reduce((a, b) => a + b)));
+        const avgScore = games.reduce((sum, g) => sum + g.scores.reduce((a, b) => a + b), 0) / totalGames;
+
+        response.json({ totalGames, bestScore, avgScore: avgScore.toFixed(2) });
+    } catch (err) {
+        response.status(500).json({ error: err.message });
+    }
+};
